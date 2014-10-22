f0b68b588caf5164f49c38a071fbf6f27f215e40
hadoop <hadoop@hadoop-master01.localdomain>
2014-10-22 21:24:10 +0900

    update sql.

--- sql/managedbcreate.sql
+++ sql/managedbcreate.sql
@@ -4,7 +4,7 @@ DROP USER equipmanage;
 CREATE USER equipmanage CREATEDB CREATEUSER;
 CREATE DATABASE equipmanage OWNER equipmanage ENCODING 'UTF8';
 
-\c equipmanage;
+\c equipmanage equipmanage;
 
 CREATE TABLE equipmanage (
      src_loc_id text,
@@ -20,5 +20,20 @@ CREATE TABLE equipmanage (
 )
 WITH (fillfactor=90);
 
-CREATE INDEX idx_src_equip_host ON equipmanage (src_equip_host);
-CREATE INDEX idx_dst_equip_host ON equipmanage (dst_equip_host);
+CREATE INDEX idx_em_src_equip_host ON equipmanage (src_equip_host);
+CREATE INDEX idx_em_dst_equip_host ON equipmanage (dst_equip_host);
+
+CREATE TABLE equiptree (
+     src_loc_id text,
+     src_equip_name text,
+     src_equip_host text,
+     src_equip_status text,
+     dst_loc_id text,
+     dst_equip_name text,
+     dst_equip_host text,
+     dst_equip_status text
+)
+WITH (fillfactor=90);
+
+CREATE INDEX idx_et_src_equip_host ON equiptree (src_equip_host);
+CREATE INDEX idx_et_dst_equip_host ON equiptree (dst_equip_host);
